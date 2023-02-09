fa9b5ee6bbad1cf7a0afd1ce630ba05da72313a0
xulihang <xulihanghai@163.com>
2023-02-09 15:02:33 +0800

    add the user script template

---
+++ document-scanner.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Document Scanner
+// @namespace    https://www.dynamsoft.com
+// @version      0.1
+// @description  Add a document scanning button to your web pages.
+// @author       Lihang Xu
+// @match        https://www.tampermonkey.net/index.php?ext=iikm&updated=true&version=4.18.1
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
